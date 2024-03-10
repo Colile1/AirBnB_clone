3af4c4749cc3beb067233b3f5036f95835955c64
Colile1 <colilesibanda@gmail.com>
2024-03-10 23:38:00 +0200

    console file

--- models/review.py
+++ models/review.py
@@ -1,13 +1,17 @@
 #!/usr/bin/python3
-""" review module"""
-
+"""Defines the Review class."""
 from models.base_model import BaseModel
 
 
 class Review(BaseModel):
+    """Represent a review.
+
+    Attributes:
+        place_id (str): Place id.
+        user_id (str): User id.
+        text (str): text of the review.
     """
-    Review model
-    """
+
     place_id = ""
     user_id = ""
     text = ""
--- models/state.py
+++ models/state.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
-""" Class State """
-
+"""Defines the State class."""
 from models.base_model import BaseModel
 
 
 class State(BaseModel):
+    """Represent a state.
+
+    Attributes:
+        name (str): name of the state.
     """
-    Class State 
-    """
+
     name = ""
