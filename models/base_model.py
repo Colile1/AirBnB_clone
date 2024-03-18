c2194955efe047cf90e2ce5b926de2f2b31cdb74
Colile1 <colilesibanda@gmail.com>
2024-03-18 18:51:25 +0200

    removed backup rdict

--- models/base_model.py
+++ models/base_model.py
@@ -37,11 +37,11 @@ class BaseModel:
         Includes the key/value pair __class__ representing
         the class name of the object.
         """
-        rdict = self.__dict__.copy()
-        rdict["created_at"] = self.created_at.isoformat()
-        rdict["updated_at"] = self.updated_at.isoformat()
-        rdict["__class__"] = self.__class__.__name__
-        return rdict
+        return_dict = self.__dict__.copy()
+        return_dict["created_at"] = self.created_at.isoformat()
+        return_dict["updated_at"] = self.updated_at.isoformat()
+        return_dict["__class__"] = self.__class__.__name__
+        return return_dict
 
     def __str__(self):
         """Return the print/str representation of the BaseModel instance."""
