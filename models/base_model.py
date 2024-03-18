d0c6c9c6b32720de28778c4975c5cf63cb31b283
Colile1 <colilesibanda@gmail.com>
2024-03-18 19:33:32 +0200

    """

--- models/base_model.py
+++ models/base_model.py
@@ -1,19 +1,27 @@
 #!/usr/bin/python3
-"""Defines the BaseModel class."""
+"""Defines all common attributes/methods
+for other classes in the tests package."""
 import models
 from uuid import uuid4
 from datetime import datetime
 
 
 class BaseModel:
-    """Represents the BaseModel of the HBnB project."""
+    """
+    Base class for all models
+    """
 
     def __init__(self, *args, **kwargs):
-        """Initialize a new BaseModel.
-        Args:
-            *args (any): Unused.
-            **kwargs (dict): Key/value pairs of attributes.
         """
+    	Initialize the object with optional arguments.
+    	
+    	Parameters:
+    	*args: Variable length argument list.
+    	**kwargs: Arbitrary keyword arguments.
+        
+    	Returns:
+    	None
+    	"""
         tform = "%Y-%m-%dT%H:%M:%S.%f"
         self.id = str(uuid4())
         self.created_at = datetime.today()
@@ -28,20 +36,22 @@ class BaseModel:
             models.storage.new(self)
 
     def save(self):
-        """Update updated_at with the current datetime."""
+        """
+        Update updated_at with the current datetime
+        """
         self.updated_at = datetime.today()
         models.storage.save()
 
     def to_dict(self):
-        """Return the dictionary of the BaseModel instance.
-        Includes the key/value pair __class__ representing
-        the class name of the object.
         """
-        rdict = self.__dict__.copy()
-        rdict["created_at"] = self.created_at.isoformat()
-        rdict["updated_at"] = self.updated_at.isoformat()
-        rdict["__class__"] = self.__class__.__name__
-        return rdict
+        Return a dictionary representation of the object,
+        including its created_at and updated_at timestamps.
+        """
+        return_dict = self.__dict__.copy()
+        return_dict["created_at"] = self.created_at.isoformat()
+        return_dict["updated_at"] = self.updated_at.isoformat()
+        return_dict["__class__"] = self.__class__.__name__
+        return return_dict
 
     def __str__(self):
         """
